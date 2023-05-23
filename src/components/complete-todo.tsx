0725e892c10b2149d07e9a77b0fe548fbbd5848c
harshmangalam <harshdev8218@gmail.com>
2023-05-23 23:27:04 +0530

    added completed todo

--- src/components/complete-todo.tsx
+++ src/components/complete-todo.tsx
@@ -1,25 +1,36 @@
 import { component$ } from "@builder.io/qwik";
+import { Form } from "@builder.io/qwik-city";
+import { useUpdateTodo } from "~/routes";
 
-export const CompleteTodo = component$(() => {
+export const CompleteTodo = component$(({ id }: { id: string }) => {
+  const action = useUpdateTodo();
   return (
-    <form>
-      <button class="btn btn-warning btn-sm btn-circle">
-        <svg
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1.5"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-          ></path>
-        </svg>
+    <Form action={action}>
+      <input type="hidden" name="id" value={id} />
+      <button
+        class={[
+          "btn btn-warning btn-sm btn-circle",
+          { loading: action.isRunning },
+        ]}
+      >
+        {!action.isRunning && (
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 12.75l6 6 9-13.5"
+            ></path>
+          </svg>
+        )}
       </button>
-    </form>
+    </Form>
   );
 });
