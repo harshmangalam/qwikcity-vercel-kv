111ab1c9cda3fa5da2b643b8c4461645b60e76a7
harshmangalam <harshdev8218@gmail.com>
2023-05-23 20:10:35 +0530

    added todo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,37 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { CreateTodo } from "~/components/create-todo";
+import { DeleteTodo } from "~/components/delete-todo";
+import { EditTodo } from "~/components/edit-todo";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div class="max-w-md mx-auto w-full py-6">
+      <CreateTodo />
+
+      <ul class="flex flex-col space-y-2 mt-8">
+        {[...new Array(10)].map((todo) => (
+          <li class="flex items-center justify-between gap-4">
+            <p class="flex-1">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            </p>
+            <div class="flex items-center gap-2">
+              <DeleteTodo />
+              <EditTodo />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Welcome to Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };
