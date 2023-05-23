17a99207494c8c49b72f8fbf5cbafb43af0d58f5
harshmangalam <harshdev8218@gmail.com>
2023-05-23 22:09:52 +0530

    added loader

--- src/components/create-todo.tsx
+++ src/components/create-todo.tsx
@@ -28,21 +28,23 @@ export const CreateTodo = component$(() => {
         type="submit"
         class={["btn btn-square btn-primary", { loading: action.isRunning }]}
       >
-        <svg
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1.5"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 4.5v15m7.5-7.5h-15"
-          ></path>
-        </svg>
+        {!action.isRunning && (
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            ></path>
+          </svg>
+        )}
       </button>
     </Form>
   );
